df74bb97beaed36f0ad6d516674d33c76014631e
Rinay Shah <rinayshah@yahoo.com>
2018-08-06 17:10:53 -0400

    Initial commit

---
+++ Rinay's Hindu Arabic Convertor/WindowsFormsApplication14/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApplication14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(158, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert to Roman";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(217, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(154, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 172);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ShortcutsEnabled = false;
+            this.textBox2.Size = new System.Drawing.Size(217, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(158, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Convert to Hindu-Arabic ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(155, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApplication14.Properties.Resources.colosseum_ext;
+            this.ClientSize = new System.Drawing.Size(479, 360);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Rinay's Hindu Arabic Convertor/WindowsFormsApplication14/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            PrivateFontCollection customFonts = new PrivateFontCollection();
+            customFonts.AddFontFile(Application.StartupPath + "\\Roman Numerals.ttf");
+            label1.Font = new Font(customFonts.Families[0], 16, FontStyle.Regular);
+            textBox2.Font = new Font(customFonts.Families[0], 16, FontStyle.Regular);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {//Disables right click context menu
+            ContextMenu abc = new ContextMenu();
+            textBox1.ContextMenu = abc;
+        }
+
+        private string ConvertedRomanNum(int num)
+        {
+            if (num > 3999999)
+            {
+                return "Enter a smaller value";
+            }
+            //Set Roman symbols and there respective values inside an array
+            string[] romanSymbol = new string[] { "m", "cm", "d", "cd", "c", "xc", "l", "xl", "x", "Mx", "v", "Mv", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int[] romanSymbolValue = { 1000000, 900000, 500000, 400000, 100000, 90000, 50000, 40000, 10000, 9000, 5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            string RomanNumber = "";
+            int remainder = num;
+            int quotient;
+            for (int i = 0; i < romanSymbolValue.Length; i++)
+            {   //Divides the Roman symbol value with the inputed number to determine the roman number 
+                quotient = remainder / romanSymbolValue[i];
+                remainder %= romanSymbolValue[i];
+                if (quotient >= 1)
+                {
+                    for (int x = 1; x <= quotient; x++)
+                    {
+                        RomanNumber += romanSymbol[i];
+                    }
+                }
+
+            }
+            return RomanNumber;
+        }
+
+        private string ConvertedNumber(string RomanNumber)
+        {
+            string normalNum;
+            /* Since the roman value of 3,999,999 is 15 charaters long, 
+              the roman number length cannot exceed 15*/
+            if (RomanNumber.Length > 15)
+            {
+                return "Enter a smaller value";
+            }
+            //Set Roman symbols and there respective values inside an array
+            string[] romansymbols = new string[] { "m", "cm", "d", "cd", "c", "xc", "l", "xl", "x", "Mx", "v", "Mv", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int[] romansymbolvalues = { 1000000, 900000, 500000, 400000, 100000, 90000, 50000, 40000, 10000, 9000, 5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            int HinduArabic = 0;
+            for (int i = 0; i < RomanNumber.Length; i++)
+            {
+                string charater = RomanNumber.Substring(i, 1);
+                int value = romansymbolvalues[Array.IndexOf(romansymbols, charater)];
+                int valuenext = 0;
+                string nextcharater = "";
+                /* Determins the Hindu-Arabic number by mathing the inputed charaters with the 
+                roman symbol string thus giving its roman symbol value */
+                if (i < RomanNumber.Length - 1)
+                {
+                    nextcharater = RomanNumber.Substring(i + 1, 1);
+                    valuenext = romansymbolvalues[Array.IndexOf(romansymbols, nextcharater)];
+                }
+                if (value >= valuenext)
+                {
+                    HinduArabic += value;
+                }
+                else
+                {
+                    HinduArabic -= value;
+                }
+            }
+            /* Matches the output of the Hindu-Arbic to Roman converter with the 
+              input of Roman to Hindu Arabic convertor to check if the input is a valid Roamn number*/
+            normalNum = HinduArabic.ToString();
+            string rightNum = ConvertedRomanNum(Convert.ToInt32(normalNum));
+            if (RomanNumber == rightNum)
+            {
+                return normalNum;
+            }
+            else
+            {
+                return "Invalid Input";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.ShortcutsEnabled = false;
+            label1.Visible = true;
+            if (textBox1.TextLength > 7)
+            {
+                label1.Text = "Enter a smaller value";
+            }
+            else
+            {
+                if (textBox1.Text != "")
+                    label1.Text = ConvertedRomanNum(Convert.ToInt32(textBox1.Text));
+            }
+            label1.Left = centreLabels(label1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.ShortcutsEnabled = false;
+            label2.Visible = true;
+            if (textBox2.Text != "")
+                label2.Text = ConvertedNumber(textBox2.Text).ToString();
+            label2.Left = centreLabels(label2);
+
+        }
+        // Stops any alphabet and specail charaters from being entered in the textbox 
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.Handled = false;
+            }
+        }
+        // Allows only the roman symbol letters in the textbox and the backspace key everything else is disabled
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsControl(e.KeyChar) || e.KeyChar == 'M' || e.KeyChar == 'C' || e.KeyChar == 'D' || e.KeyChar == 'X' || e.KeyChar == 'L' || e.KeyChar == 'I' || e.KeyChar == 'V' || +e.KeyChar == 'm' || e.KeyChar == 'c' || e.KeyChar == 'd' || e.KeyChar == 'x' || e.KeyChar == 'l' || e.KeyChar == 'v')
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+
+
+        }
+        // Puts label 1 & label 2 in the center of the form 
+
+        private int centreLabels(Label label)
+        {
+            int x = this.Width / 2 - label.Width / 2;
+            return x;
+        }
+
+    }
+}
